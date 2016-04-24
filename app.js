caa14557db7de6ad510a5f05430e4944bf43a6b9
Brandon Barber <roecrew@gmail.com>
2016-04-24 12:43:41 -0500

    0.1.2
    minor

--- app.js
+++ app.js
@@ -2,10 +2,7 @@
 
 var hashtable = require('./index.js')
 var tmp = {"a": "apple", "b": "banana"};
-hashtable.store("hello")
-hashtable.store(4)
 hashtable.store(tmp)
-console.log(hashtable.fetch("hello"));
-console.log(hashtable.fetch(4));
-console.log(hashtable.fetch(tmp)["b"]);
+console.log(hashtable.fetch("a"));
+console.log(hashtable.hasitem("d"));
 console.log("Size: "+hashtable.size());
